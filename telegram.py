36f3bf1f9d711af8c70692d89f23b307a6ba7fa2
John S John <johnsabujohn@gmail.com>
2021-05-20 17:23:26 +0530

    Added integration for telegram

--- telegram.py
+++ telegram.py
@@ -3,7 +3,7 @@ import telebot
 from telethon.sync import TelegramClient
 from telethon.tl.types import InputPeerUser, InputPeerChannel
 from telethon import TelegramClient, sync, events
-
+from telethon import utils
 # get your api_id, api_hash, token
 # from telegram as described above
 api_id = '5367299'
@@ -13,34 +13,41 @@ token = '1732626920:AAH9ar3USEHyHsVApvx4f5NJnx8QFJdfqoo'
 # your phone number
 phone = '+919790986756'
 
+
 # creating a telegram session and assigning
 # it to a variable client
-client = TelegramClient('session', api_id, api_hash)
+def get_client():
+    client = TelegramClient('session', api_id, api_hash)
 
-# connecting and building the session
-client.connect()
+    # connecting and building the session
+    client.connect()
 
-# in case of script ran first time it will
-# ask either to input token or otp sent to
-# number or sent or your telegram id
-if not client.is_user_authorized():
-    client.send_code_request(phone)
+    # in case of script ran first time it will
+    # ask either to input token or otp sent to
+    # number or sent or your telegram id
+    if not client.is_user_authorized():
+        client.send_code_request(phone)
 
-    # signing in the client
-    client.sign_in(phone, input('Enter the code: '))
+        # signing in the client
+        client.sign_in(phone, input('Enter the code: '))
+    print('Telegram connected successfully')
+    return client
 
-try:
-    # receiver user_id and access_hash, use
-    # my user_id and access_hash for reference
-    receiver = InputPeerUser('user_id', 'user_hash')
 
-    # sending message using telegram client
-    client.send_message(receiver, 'asd', parse_mode='html')
-except Exception as e:
+def send_message(user_id, client, message):
+    try:
+        # receiver user_id and access_hash, use
+        # my user_id and access_hash for reference
+        entity = client.get_input_entity(user_id)
+        # receiver = InputPeerUser('user_id', 'user_hash')
+        receiver = utils.get_input_user(entity)
+        # sending message using telegram client
+        client.send_message(receiver, message, parse_mode='html')
+    except Exception as e:
 
-    # there may be many error coming in while like peer
-    # error, wwrong access_hash, flood_error, etc
-    print(e);
+        # there may be many error coming in while like peer
+        # error, wwrong access_hash, flood_error, etc
+        print(e)
 
 # disconnecting the telegram session
-client.disconnect()
+# client.disconnect()
--- vaxxine.py
+++ vaxxine.py
@@ -2,16 +2,15 @@ import time
 
 import requests
 import datetime
-import telebot
-import telethon
+
+from telegram import get_client, send_message
 
 url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=307&date='
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+client = get_client()
 while True:
     currDate = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%d-%m-%Y")
-    print(currDate)
-    url = url+ currDate
-    print(url)
+    url = url + currDate
     result = requests.get(url, headers=headers)
     if result.status_code == 200:
         centers = result.json().get('centers')
@@ -25,11 +24,17 @@ while True:
                     failedString = 'No Slots available in ' + centerName + ' on ' + currDate
                     print(failedString)
                 if min_age_limit == 18 and available_capacity_dose1 > 0:
-                    successString = str(available_capacity_dose1) + ' slots available in ' + centerName + ' on ' + currDate + ' for 18-45'
+                    successString = str(available_capacity_dose1) + ' slots available in ' + centerName + ' on ' + currDate + ' for 18-45.'
+                    successString = successString + '. Goto ' + 'https://selfregistration.cowin.gov.in/'
                     print(successString)
+                    send_message(message=successString, client=client, user_id='@johnsabujohn')
+
                 if min_age_limit == 45 and available_capacity_dose1 > 0:
                     successString = str(available_capacity_dose1) + ' slots available in ' + centerName + ' on ' + currDate + ' for 45+'
+                    successString = successString + '. Goto ' + 'https://selfregistration.cowin.gov.in/'
                     print(successString)
+                    send_message(message=successString, client=client, user_id='@johnsabujohn')
     else:
         print('not found')
+
     time.sleep(5)
